1c53a45205a20aed565846a08f7fc4179d572050
131c2 <bg3189@naver.com>
2025-06-03 04:25:25 +0900

    React

---
+++ React/react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    //return 되는 요소는 반드시 1개.
+    <div>
+      <h1>Hello</h1>
+      <h2>Hello2</h2>      
+    </div>
+  );
+}
+
+export default App;
